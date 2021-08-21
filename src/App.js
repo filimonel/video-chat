73d85d10c40b2f7dda77eb9a7ff8e9f092cc4c57
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-08-21 18:50:23 +1000

    added component files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
 
 const App = () => {
-  return <div>Video Chat App</div>;
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      {/* Video Player */}
+      {/* Options > Notifications */}
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
+import ReactDom from "react-dom";
 
 import App from "./App";
 import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
