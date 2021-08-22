5f38e5832c2fc751946fde7d44f64d1453358f6e
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-08-22 20:35:28 +1000

    server-client connection bug is fixed

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,34 +1,35 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import {
   Button,
   TextField,
   Grid,
   Typography,
   Container,
+  Paper,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import { Assignment, Phone, PhoneDisables } from '@material-ui/icons'
+import { Assignment, Phone, PhoneDisabled } from "@material-ui/icons";
 
 import { SocketContext } from "../SocketContext";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
   },
   gridContainer: {
-    width: '100%',
-    [theme.breakpoints.down('xs')]: {
-      flexDirection: 'column',
+    width: "100%",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
     },
   },
   container: {
-    width: '600px',
-    margin: '35px 0',
+    width: "600px",
+    margin: "35px 0",
     padding: 0,
-    [theme.breakpoints.down('xs')]: {
-      width: '80%',
+    [theme.breakpoints.down("xs")]: {
+      width: "80%",
     },
   },
   margin: {
@@ -38,17 +39,90 @@ const useStyles = makeStyles((theme) => ({
     padding: 20,
   },
   paper: {
-    padding: '10px 20px',
-    border: '2px solid black',
+    padding: "10px 20px",
+    border: "2px solid black",
   },
 }));
 
 const Options = ({ children }) => {
+  // Access socketcontext values and functions
+  const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } =
+    useContext(SocketContext);
+  // Set id so we can initiate call
+  const [idToCall, setIdToCall] = useState("");
+  // Access styles from above
+  const classes = useStyles();
+
   return (
-    <div>
-      Options
-      {children}
-    </div>
+    <Container className={classes.container}>
+      <Paper elevation={10} className={classes.paper}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">
+                Account Info
+              </Typography>
+              <TextField
+                label="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+              />
+              {console.log(me)}
+              <CopyToClipboard text={me} className={classes.margin}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  startIcon={<Assignment fontSize="large" />}
+                >
+                  Copy Your ID
+                </Button>
+              </CopyToClipboard>
+            </Grid>
+            {/* Make A Call */}
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">
+                Make a call
+              </Typography>
+              <TextField
+                label="ID to call"
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+                fullWidth
+              />
+              {callAccepted && !callEnded ? (
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  startIcon={<PhoneDisabled fontSize="large" />}
+                  fullWidth
+                  // Using leavecall function from SocketContext to end call onClick
+                  onClick={leaveCall}
+                  className={classes.margin}
+                >
+                  Hang Up
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<Phone fontSize="large" />}
+                  fullWidth
+                  // Using callUser function from SocketContext to start a call onClick
+                  // () => is important, using a normal function, it will not run async.
+                  onClick={() => callUser(idToCall)}
+                  className={classes.margin}
+                >
+                  Call
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </form>
+        {children}
+      </Paper>
+    </Container>
   );
 };
 
